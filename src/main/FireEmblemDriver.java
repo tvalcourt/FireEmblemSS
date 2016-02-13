2eea36f1c8d42bf45b842cf088471676fb841986
Trevor <tvalcourt7@yahoo.com>
2016-02-13 13:48:48 -0500

    Initial project commit, setup base Scene

---
+++ src/main/FireEmblemDriver.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FireEmblemDriver extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../ui/main.fxml"));
+        primaryStage.setTitle("Fire Emblem Guide");
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
